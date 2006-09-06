2add1f4c98ecb32e49a948339862695db28c895d
Hans Johnson <hans-johnson@uiowa.edu>
2006-09-06 18:14:39 +0000

    This commit was manufactured by cvs2git to create tag 'release-0_4'.

--- Matlibs/matlab/@file_array/ctranspose.m
+++
@@ -1,9 +0,0 @@
-function varargout = ctranspose(varargin)
-% Transposing not allowed
-% _______________________________________________________________________
-% Copyright (C) 2005 Wellcome Department of Imaging Neuroscience
-
-%
-% $Id$
-
-error('file_array objects can not be transposed.');
