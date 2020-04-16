fc36400416f8951f675afddab9e8b56628219e10
leej3 <johnleenimh@gmail.com>
2020-04-16 13:59:18 -0400

    tidy installation pattern.
    Tidies support for namespaced targets as part of superbuild, now adding
    the aliased library within the add_nifti_library function.
    
    All functionality required for ITK to drive the build now working. i.e.
    full supports the NIFTI_INSTALL_NO... vars.
    
    Manages all header file installation using target properties. Either
    PUBLIC_HEADER or PRIVATE_HEADER can be used depending on whether the
    header should be installed.
    
    For cmake >=3.13, we now aggregate the list of targets to be
    installed and install them all in the main cmake file to reduce
    amount of code required and to reduce the chance of errors of
    installation directory names etc.
    
    For older cmake versions the above pattern for target installation is
    not possible because targets are not scoped beyond the directory in
    which they are defined. The approach taken for these cmake versions is
    to use the `install_nifti_target` function in the targets cmake file
    (after all target properties etc. have been set). These lines can be
    dropped in the future.

--- cmake/nifti_macros.cmake
+++ cmake/nifti_macros.cmake
@@ -47,3 +47,58 @@ function(add_nifti_executable target_in)
     set_property(GLOBAL PROPERTY installed_targets "${tmp}")
   endif()
 endfunction()
+
+function(install_nifti_target target_name)
+  # Check if the current directory is the base directory of the project
+  if("${PROJECT_SOURCE_DIR}" STREQUAL "${CMAKE_CURRENT_LIST_DIR}")
+    set(IS_PROJECT_DIR 1)
+  else()
+    set(IS_PROJECT_DIR 0)
+  endif()
+
+  if(NOT INSTALL_CONFIG_SUPPORTED AND IS_PROJECT_DIR)
+    # Early version of CMake so installation must happen in the directory in
+    # which the target is defined. No installation occurs from the project
+    # directory
+    return()
+  elseif(INSTALL_CONFIG_SUPPORTED AND NOT IS_PROJECT_DIR)
+    # CMake >=3.13 has support for referencing targets in parent scopes of the
+    # one in which the target is defined. This enables a central management of
+    # the installation process, along with installating an target export set.
+    # No installation occurs from the directory in which the target is defined
+    return()
+  endif()
+
+  # Install the targets now that the appropriate directory is confirmed.
+  if(NIFTI_INSTALL_NO_DEVELOPMENT)
+    install(TARGETS ${target_name}
+            EXPORT ${NIFTI_INSTALL_EXPORT_NAME}
+            RUNTIME
+              COMPONENT RuntimeBinaries
+              DESTINATION ${NIFTI_INSTALL_RUNTIME_DIR}
+            ARCHIVE
+              DESTINATION ${NIFTI_INSTALL_LIBRARY_DIR}
+              COMPONENT RuntimeLibraries
+            LIBRARY
+              DESTINATION ${NIFTI_INSTALL_LIBRARY_DIR}
+              COMPONENT RuntimeLibraries
+            )
+  else()
+    install(TARGETS ${target_name}
+            EXPORT ${NIFTI_INSTALL_EXPORT_NAME}
+            RUNTIME
+              COMPONENT RuntimeBinaries
+              DESTINATION ${NIFTI_INSTALL_RUNTIME_DIR}
+            ARCHIVE
+              DESTINATION ${NIFTI_INSTALL_LIBRARY_DIR}
+              COMPONENT RuntimeLibraries
+            LIBRARY
+              DESTINATION ${NIFTI_INSTALL_LIBRARY_DIR}
+              COMPONENT RuntimeLibraries
+            PUBLIC_HEADER
+              DESTINATION ${NIFTI_INSTALL_INCLUDE_DIR}
+            INCLUDES
+              DESTINATION ${NIFTI_INSTALL_INCLUDE_DIR}
+            )
+    endif()
+endfunction()
